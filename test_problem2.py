d7f4ffe3f556fb8e424d30936cb08396cf7b05a5
tabinks <abinkowski@uchicago.edu>
2020-11-10 13:50:07 -0600

    Update test_problem2.py

--- test_problem2.py
+++ test_problem2.py
@@ -1,6 +1,9 @@
 import pytest
 from fraction import Fraction
-  
+
+def test_input_output():
+    print("Input Output Test")
+    
 def test_initialization():
     """Test fraction creation"""
     f1 = 0.625
