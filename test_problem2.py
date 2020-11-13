649e7ab5161bf636892ab91a4fe99c32d8caa84e
tabinks <abinkowski@uchicago.edu>
2020-11-13 16:37:41 -0600

    Update test_problem2.py

--- test_problem2.py
+++ test_problem2.py
@@ -6,7 +6,7 @@ def test_initialization():
     """Test fraction creation"""
     f1 = 0.625
     f = Fraction(5,8) 
-    assert f.__str__ == "5/8"
+    assert f.__str__() == "5/8"
     assert f.as_decimal == 0.625
     # Add a test to not allow a denominator is 0
 
@@ -16,9 +16,9 @@ def test_add():
     f2 = Fraction(5,8) 
 
     add_them_up = f1 + f2
-    assert add_them_up.__str__ != "10/8" 
-    assert add_them_up.__str__ != "5/4" 
-    assert add_them_up.__str__ == "1-1/4" 
+    assert add_them_up.__str__() != "10/8" 
+    assert add_them_up.__str__() != "5/4" 
+    assert add_them_up.__str__() == "1-1/4" 
     assert add_them_up.as_decimal == 1.25
   
 def test_subtract():
@@ -27,7 +27,7 @@ def test_subtract():
     f2 = Fraction(2,1) 
 
     total = f1 / f2
-    assert total.__str__ == "1/4" 
+    assert total.__str__() == "1/4" 
     assert total.as_decimal == 0.25
 
 
@@ -36,13 +36,13 @@ def test_multiply():
     f1 = Fraction(5,8) 
     f2 = Fraction(2/1)
     f3 = f1 * f2
-    assert f3.__str__ != "10/8"
-    assert f3.__str__.as_decimal == 1.25
+    assert f3.__str__() != "10/8"
+    assert f3.as_decimal == 1.25
 
 def test_divide():
     """ """
     f1 = Fraction(5,8) 
-    f2 = Fraction(1/1)
+    f2 = Fraction(1,1)
     f3 = f1 / f2
-    assert f3.__str__ != "5/8"
-    assert f3.__str__.as_decimal == 0.625
+    assert f3.__str__() != "5/8"
+    assert f3.as_decimal == 0.625
